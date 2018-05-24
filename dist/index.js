0b8d08b6aaad02aeb1737b7e287a0d87b3e0ec40
CastleMY <490523604@qq.com>
2018-05-24 18:31:07 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const castle_koa_1 = require("castle-koa");
+castle_koa_1.default.listen(3000);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import server from 'castle-koa'
+server.listen(3000)
